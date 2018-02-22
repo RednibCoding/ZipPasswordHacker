bc12ba8b8c72d0cd7dc7cb2dcbe6d8f07a21f9f3
Michael Binder <binder.mich@gmx.de>
2018-02-22 17:49:32 +0100

    Update ZipPasswordHacker.py
    Changed comments

--- ZipPasswordHacker.py
+++ ZipPasswordHacker.py
@@ -1,15 +1,17 @@
 
-# Zip dictonary password hacker
-# 03.12.2016
-# Michael Binder
-#
-# Uses a dictonary ("passwords.txt") where the passwords are stored
-# to hack the password of a zip file ("test.zip")
-# it tries all passwords in the "passwords.txt" to hack the zip file
-# try different password files if the "passwords.txt" does not contain the correct password
-# you can get password files at https://wiki.skullsecurity.org/index.php?title=Passwords
-# or just google for: dictonary password list or dictonary password files or Password dictionaries
-# ore something like that
+# ZipPasswordHacker
+
+# 03.12.2016 by Michael Binder
+
+# Python 3.5x or higher has to be installed
+
+# For more details visit: https://codeconform.blogspot.de/2018/02/python-passwort-cracker-fur-zip-archive.html
+
+# Uses a dictonary ("passwords.txt") where the passwords are stored to hack the password of a zip file "test.zip" .
+
+# It tries all passwords in the "passwords.txt" to hack the zip file
+
+# You can get password files at https://wiki.skullsecurity.org/index.php?title=Passwords Or google for them.
 
 
 
